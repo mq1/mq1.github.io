97bfb317667bf87fb3ba67ec91e60f43fbe20088
Manuel Quarneti <manuelquarneti@gmail.com>
2021-03-20 19:06:45 +0100

    minify html only on production

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const htmlmin = require("html-minifier");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if (outputPath.endsWith(".html")) {
+    if (outputPath.endsWith(".html") && process.env.NODE_ENV === "production") {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
