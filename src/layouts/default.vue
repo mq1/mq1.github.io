49cc99d49157d4c09eeab5c8ad450708998462a6
Manuel Quarneti <manuelquarneti@gmail.com>
2021-05-20 10:48:56 +0200

    footer styling

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -62,11 +62,19 @@ const { t, locale } = useI18n()
         <router-view />
       </main>
 
-      <footer class="text-center">
-        MIT Licensed |
-        © 2021 Manuel Quarneti |
-        Hosted on Netlify |
-        Source code <a class="underline" href="https://github.com/mq1/mq1.eu">here</a>
+      <footer class="flex items-center justify-center divide-x-2">
+        <div class="px-2 flex items-center">
+          MIT Licensed
+        </div>
+        <div class="px-2 flex items-center">
+          © 2021 Manuel Quarneti
+        </div>
+        <div class="px-2 flex items-center">
+          Hosted on Netlify
+        </div>
+        <a class="px-2 flex items-center" href="https://github.com/mq1/mq1.eu">
+          <carbon-logo-github />
+        </a>
       </footer>
     </div>
   </div>
