05d8de0e41d85b22ba86893288758aa5e3b32cf6
Manuel Quarneti <manuelquarneti@gmail.com>
2021-06-03 16:12:15 +0200

    dark theme

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="dark:bg-black dark:text-white">
-    <div class="flex flex-col justify-between gap-y-8 min-h-screen max-w-7xl mx-auto p-4 sm:p-8">
-      <Header />
+  <div class="flex flex-col justify-between gap-y-8 min-h-screen max-w-7xl mx-auto p-4 sm:p-8">
+    <Header />
 
-      <main class="flex flex-col gap-y-4">
-        <router-view />
-      </main>
+    <main class="flex flex-col gap-y-4">
+      <router-view />
+    </main>
 
-      <Footer />
-    </div>
+    <Footer />
   </div>
 </template>
