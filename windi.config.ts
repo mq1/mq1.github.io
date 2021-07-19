d0730a10864ae1e1925eb0dcd51653397e719a1f
Manuel Quarneti <manuelquarneti@gmail.com>
2021-07-19 20:00:10 +0200

    styling

--- windi.config.ts
+++ windi.config.ts
@@ -4,9 +4,6 @@ import typography from 'windicss/plugin/typography'
 
 export default defineConfig({
   darkMode: 'class',
-  // https://windicss.org/posts/v30.html#attributify-mode
-  attributify: true,
-
   plugins: [
     typography(),
   ],
