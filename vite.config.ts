def83d0b498af12220bc66c0e05f906a3a6bc5b0
Manuel Quarneti <manuelquarneti@gmail.com>
2021-09-15 11:59:27 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,42 +3,81 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import WindiCSS from 'vite-plugin-windicss'
-import Components from 'vite-plugin-components'
-import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
+import Inspect from 'vite-plugin-inspect'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-
   plugins: [
     Vue(),
+
+    // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
+
+    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
     Layouts(),
-    WindiCSS(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+      ],
+      dts: true,
+    }),
+
+    // https://github.com/antfu/unplugin-vue-components
     Components({
-      globalComponentsDeclaration: true,
-      customComponentResolvers: [
-        ViteIconsResolver({
+      dts: true,
+
+      // custom resolvers
+      resolvers: [
+        // auto import icons
+        // https://github.com/antfu/unplugin-icons
+        IconsResolver({
           componentPrefix: '',
           enabledCollections: ['feather'],
         }),
       ],
     }),
-    ViteIcons(),
 
-    // https://github.com/intlify/vite-plugin-vue-i18n
+    // https://github.com/antfu/unplugin-icons
+    Icons(),
+
+    // https://github.com/antfu/vite-plugin-windicss
+    WindiCSS(),
+
+    // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
     VueI18n({
       runtimeOnly: true,
       compositionOnly: true,
       include: [path.resolve(__dirname, 'locales/**')],
     }),
+
+    // https://github.com/antfu/vite-plugin-inspect
+    Inspect({
+      // change this to enable inspect for debugging
+      enabled: false,
+    }),
   ],
+
+  server: {
+    fs: {
+      strict: true,
+    },
+  },
 
   // https://github.com/antfu/vite-ssg
   ssgOptions: {
