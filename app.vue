ea56dba3ae7c5aff128bc69273b76efa204bdd51
Manuel Quarneti <manuelquarneti@gmail.com>
2021-12-22 15:45:08 +0100

    add meta tags

--- app.vue
+++ app.vue
@@ -1,4 +1,11 @@
 <template>
+  <Html lang="en">
+    <Head>
+      <Title>Manuel Quarneti</Title>
+      <Meta name="description" content="Manuel Quarneti's personal website" />
+    </Head>
+  </Html>
+
   <div h-screen flex flex-col items-center justify-between p-4>
     <div></div>
     <div>
