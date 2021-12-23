44120530f783be35455992c92ecb16de9b93cc89
Manuel Quarneti <manuelquarneti@gmail.com>
2021-12-23 10:03:56 +0100

    style fixes

--- app.vue
+++ app.vue
@@ -17,7 +17,7 @@
           <a href="mailto:manuelquarneti@gmail.com" i-feather-mail></a>
         </div>
       </div>
-      <div flex flex-col sm:flex-row gap-x-8 gap-y-2 bg-gradient-to-r from-red-500 to-yellow-500 text-white p-8 m-4 rounded-3xl shadow-xl>
+      <div flex flex-col sm:flex-row justify-between gap-2 bg-gradient-to-r from-red-500 to-yellow-500 text-white p-8 m-4 rounded-3xl shadow-xl>
         <a underline underline-3 underline-white-500 href="https://mq1.github.io/mq1/en.pdf">Download my CV in English</a>
         <a underline underline-3 underline-white-500 href="https://mq1.github.io/mq1/it.pdf">Scarica il mio CV in Italiano</a>
       </div>
