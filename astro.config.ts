a9aeabd2670c8fa66a773bbee9d48e43d9a8c498
Manuel Quarneti <manuelquarneti@gmail.com>
2022-03-28 15:38:13 +0200

    use turbolinks

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import turbolinks from "@astrojs/turbolinks";
 
 export default defineConfig({
+  integrations: [turbolinks()],
   buildOptions: {
     site: "https://mq1.eu/",
   },
