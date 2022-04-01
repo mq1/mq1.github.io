1c2bdf780de093d32df9273543e2ae8b0b8f3b80
Manuel Quarneti <manuelquarneti@gmail.com>
2022-04-01 10:48:58 +0200

    remove turbolinks

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import turbolinks from "@astrojs/turbolinks";
 
 export default defineConfig({
-  integrations: [tailwind({}), turbolinks()],
+  integrations: [tailwind({})],
   buildOptions: {
     site: "https://mq1.eu/",
   },
