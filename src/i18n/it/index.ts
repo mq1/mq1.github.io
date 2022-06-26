f9077d48628c221a044e199d81dc22b2efd58cdd
Manuel Quarneti <manuelquarneti@gmail.com>
2022-06-26 21:16:39 +0200

    ✨ Use typesafe-i18n

---
+++ src/i18n/it/index.ts
@@ -0,0 +1,12 @@
+import type { Translation } from "../i18n-types.js";
+
+const it: Translation = {
+  NAME: "🇮🇹 Italian",
+  DESCRIPTION: "Sito personale di Manuel Quarneti",
+  HI: "👋 Ciao, sono Manuel Quarneti",
+  DOWNLOAD_CV: "📄 Scarica il mio CV",
+  PROJECTS: "🗂️ Progetti",
+  SOURCE_CODE: "Codice sorgente",
+};
+
+export default it;
--- src/translations/index.ts
+++
@@ -1,7 +0,0 @@
-import en from "./en";
-import it from "./it";
-
-export const locales = ["en", "it"] as const;
-export type Locale = typeof locales[number];
-
-export const translations = { en, it };
