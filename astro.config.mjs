b52a5aba4883fa54315cb46e0f9c1181b2750ef7
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-19 19:31:33 +0200

    ⚡️ Add @astrojs/prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), prefetch()]
 });
