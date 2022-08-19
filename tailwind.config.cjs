94defd65f0ec4f4e0ac33929389525f3d236b0f5
Manuel Quarneti <manuelquarneti@gmail.com>
2022-08-19 11:28:57 +0200

    ✨ Rebuild site

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
