76c57cf253571609b9cc72f423ca92f66750f3a4
Manuel Quarneti <manuelquarneti@gmail.com>
2023-02-21 10:34:10 +0100

    🎉 Rewrite

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-
-
-export default defineConfig({
-  integrations: [tailwind(), image()]
-});
