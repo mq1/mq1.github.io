0abe3fe83cf19929462135319eeb953bc94a5f91
Manuel Quarneti <mq1@ik.me>
2025-09-16 09:51:45 +0200

    🍱 Add org.flathub.VerifiedApps.txt

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -2,4 +2,7 @@ export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "node_modules/@picocss/pico/css/pico.fluid.classless.purple.min.css": "pico.css"
     });
+    eleventyConfig.addPassthroughCopy({
+        "assets/org.flathub.VerifiedApps.txt": ".well-known/org.flathub.VerifiedApps.txt"
+    });
 };
