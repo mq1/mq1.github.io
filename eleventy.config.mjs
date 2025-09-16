f910ece3d7ca3b990e42bd5f016fb71a21224970
Manuel Quarneti <mq1@ik.me>
2025-09-16 10:24:30 +0200

    Revert "👷 Add nojekyll"
    This reverts commit 2f4d63ea9c8f63fcb52cd44761077724df17e80a.

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,4 @@
 export default async function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy(".nojekyll");
     eleventyConfig.addPassthroughCopy({
         "node_modules/@picocss/pico/css/pico.fluid.classless.purple.min.css": "pico.css"
     });
