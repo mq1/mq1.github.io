81c212543bf587fcfd7851c303244487af0752d3
Manuel Quarneti <mq1@ik.me>
2025-09-16 10:08:50 +0200

    Revert "👷 Add _config.yml"
    This reverts commit 8a7d046a30d1df36fd97cba54460457bff743eaf.

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -3,9 +3,6 @@ export default async function (eleventyConfig) {
         "node_modules/@picocss/pico/css/pico.fluid.classless.purple.min.css": "pico.css"
     });
     eleventyConfig.addPassthroughCopy({
-        "assets/_config.yml": "_config.yml"
-    });
-    eleventyConfig.addPassthroughCopy({
         "assets/org.flathub.VerifiedApps.txt": ".well-known/org.flathub.VerifiedApps.txt"
     });
 };
